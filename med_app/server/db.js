664da374aff0b24be6e0773020434716c325abd5
Turnt-11 <kylesvault@gmail.com>
2024-10-22 19:39:38 -0400

    changed db credentials

--- med_app/server/db.js
+++ med_app/server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:lIOxL7ci3MkJaa0pgsWUTU0Q@172.21.188.51:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
