c0d819cd36de8f44e2476b5b8da550fcbffc2d8e
Turnt-11 <kylesvault@gmail.com>
2024-10-22 21:24:20 -0400

    partially done creating react project components

---
+++ med_app/src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css';
+
+<nav className="navbar">
+        <div className="logo">
+            StayHealthy
+        </div>
+        <ul className="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Appointments</a></li>
+            <li><a href="#">Health Blog</a></li>
+            <li><a href="#">Reviews</a></li>
+        </ul>
+        <div className="auth-buttons">
+            <a href="../Sign_Up/Sign_Up.html" className="pill-button signup-btn">Sign Up</a>
+            <a href="../Login/Login.html" className="pill-button login-btn">Login</a>
+        </div>
+    </nav>
