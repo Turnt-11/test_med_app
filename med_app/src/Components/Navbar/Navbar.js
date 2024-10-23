2c1538e30c9b12bab2dbf36c32f5034b9d23e440
turnt-11 <kylesvault@gmail.com>
2024-10-23 14:54:46 -0400

    getting components built and linked

--- med_app/src/Components/Navbar/Navbar.js
+++ med_app/src/Components/Navbar/Navbar.js
@@ -1,17 +1,25 @@
 import './Navbar.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+function Navbar() {
 
-<nav className="navbar">
-        <div className="logo">
-            StayHealthy
-        </div>
-        <ul className="nav-links">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Appointments</a></li>
-            <li><a href="#">Health Blog</a></li>
-            <li><a href="#">Reviews</a></li>
-        </ul>
-        <div className="auth-buttons">
-            <a href="../Sign_Up/Sign_Up.html" className="pill-button signup-btn">Sign Up</a>
-            <a href="../Login/Login.html" className="pill-button login-btn">Login</a>
-        </div>
-    </nav>
+    return(
+        <nav className="navbar">
+            <div className="logo">
+                StayHealthy
+            </div>
+            <ul className="nav-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="#">Appointments</a></li>
+                <li><a href="#">Health Blog</a></li>
+                <li><a href="#">Reviews</a></li>
+            </ul>
+            <div className="auth-buttons">
+                <a href="../Sign_Up/Sign_Up.html" className="pill-button signup-btn">Sign Up</a>
+                <a href="../Login/Login.html" className="pill-button login-btn">Login</a>
+            </div>
+        </nav>);
+
+    }
+
+    export default Navbar;
