1e9fabd7571102a1e93d3f8964aba4836a40a850
kyle <kylesvault@gmail.com>
2024-10-30 09:44:55 -0400

    final test

--- med_app/server/db.js
+++ med_app/server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:uqfyk3wV4i5Kz1AjmWSHIUQb@172.21.141.164:27017";
+const mongoURI =  "mongodb://root:ufKn3TEqi0qBd0pFvXxpJLSr@172.21.175.168:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
